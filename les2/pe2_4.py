d9f2ca89b6f030ce535af61b44872256987381c4
ramonjaspers <ramonj360@gmail.com>
2017-09-11 11:27:52 +0200

    Opdrachten van les 2 afgemaakt

---
+++ les2/pe2_4.py
@@ -0,0 +1,11 @@
+Uurloon = int(input("Wat is je uurloon?: "))
+Werkuren = int(input("Hoeveel uur heb je gewerkt: "))
+
+berekening = Werkuren*Uurloon
+
+
+print (str(Werkuren)+ ' uur levert je ' +str(berekening)+' euro op')
+
+
+
+
